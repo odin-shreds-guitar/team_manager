adcd9776834f3928f67eb9891c71102bbc8edbfe
fabriziospurio <90812244+odin-shreds-guitar@users.noreply.github.com>
2021-12-12 18:20:03 -0500

    game links (all going to the same)

--- client/src/views/Status.js
+++ client/src/views/Status.js
@@ -11,16 +11,15 @@ const Status = () => {
 		<div className='wrapper'>
 			{/* This is the same everywhere */}
 			<div className="links-bar">
-				< Link to="/players/list">List</Link>
-				< Link to="/players/addplayer">Add Player</Link>
+				< Link to="/status/game/1">Game 1</Link>
+				< Link to="/status/game/1">Game 2</Link>
+				< Link to="/status/game/1">Game 3</Link>
 			</div>
 			{/* < Add Player component here */}
 			<h2>Add Player</h2>
 			< PlayersList 
 				player= { player }
 				setPlayer =  { setPlayer }
-				position= { position }
-				setPosition = { setPosition }
 			/>
 		</div>
 	)
